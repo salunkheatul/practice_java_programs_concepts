bbbeb7b215c73dd70638aafe961b93b3c61fc113
atul <atul@greyatom.com>
2020-06-22 15:21:41 +0530

    added concepts

--- src/main/java/concepts/WideningPrimitiveConversion.java
+++ src/main/java/concepts/WideningPrimitiveConversion.java
@@ -20,11 +20,57 @@ public class WideningPrimitiveConversion {
         out.println('O');
     }
 
+    public void print4(Short a, Short b) {
+        out.print("print 4  ====> ");
+        out.println(a + b);
+    }
+
+    public void print5(byte a, byte b) {
+        out.print("print 5  ====> ");
+        out.println(a + b);
+    }
+
+    public void print6(double a, double b) {
+        out.print("print 6  ====> ");
+        out.print(a + b);
+        out.print("   ");
+        out.println(a + Double.valueOf(b).intValue());
+    }
+
+    public void print7(float a, float b) {
+        out.print("print 6  ====> ");
+        out.print(a + b);
+        out.print("   ");
+        out.println(a + Float.valueOf(b).intValue());
+    }
+
+    public void print8(long a, long b) {
+        out.print("print 6  ====> ");
+        out.print(a + b);
+        out.print("   ");
+        out.println(a + Long.valueOf(b).intValue());
+    }
+
     public static void main(String[] args) {
         WideningPrimitiveConversion wideningPrimitiveConversion = new WideningPrimitiveConversion();
         wideningPrimitiveConversion.print1();
         wideningPrimitiveConversion.print2();
         wideningPrimitiveConversion.print3();
+        short a = 10;
+        short b = 30;
+        wideningPrimitiveConversion.print4(a, b);
+        byte p = 100;
+        byte q = 100;
+        wideningPrimitiveConversion.print5(p, q);
+        double r = 1.99;
+        double s = 1.99;
+        wideningPrimitiveConversion.print6(r, s);
+        float x = 125.250f;
+        float y = 100.250f;
+        wideningPrimitiveConversion.print7(x, y);
+        long m = 1500L;
+        long n = 1500L;
+        wideningPrimitiveConversion.print8(m, n);
     }
 }
 
@@ -33,5 +79,10 @@ public class WideningPrimitiveConversion {
 print 1  ====> YO
 print 2  ====> 155
 print 3  ====> L  O
+print 4  ====> 40
+print 5  ====> 200
+print 6  ====> 3.98   2.99
+print 6  ====> 225.5   225.25
+print 6  ====> 3000   3000
 
  */
